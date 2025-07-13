a28c26e918761dfbfc47a3441e34aa3228fee3ef
Najrul Islam <mdnajrulislam.4738@gmail.com>
2025-07-13 11:38:43 +0600

    set server error handling process

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ const startServer = async () => {
 
     console.log("Connected to Database.");
 
-    app.listen(port, () => {
+    server = app.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
     });
   } catch (error) {
@@ -28,3 +28,70 @@ const startServer = async () => {
 };
 
 startServer();
+
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection detected... Server shutting down.", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(
+    "uncaught Exception error detected... Server shutting down.",
+    err
+  );
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+// sigterm signal // signal from cloud companies like AWS, Vercel
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal received... Server shutting down.");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+// sigint signal // signal from ctrl + c
+process.on("SIGINT", () => {
+  console.log("SIGINT signal received... Server shutting down.");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+// unhandled rejection error example
+// Promise.reject(
+//   new Error("I forgot to catch this promise within try catch block.")
+// );
+
+//uncaught exception error example
+// throw new Error("I forgot to handle this error");
+
+/**
+ * unhandled rejection error.
+ * uncaught rejection/exception error.
+ * sigterm signal error
+ */
