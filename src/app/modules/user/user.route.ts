312609ee46fe0932c227cb66bd88c4076fc5ccc1
Najrul Islam <mdnajrulislam.4738@gmail.com>
2025-07-15 20:28:47 +0600

    create user request validator middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
+import { createUserZodSchema } from "./user.validation";
+import { validateRequest } from "../../middleware/validateRequest";
 
 const router = Router();
 
-router.post("/register", UserController.createUser);
+router.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  UserController.createUser
+);
 router.get("/all-users", UserController.getAllUsers);
 
 export const UserRoutes = router;
