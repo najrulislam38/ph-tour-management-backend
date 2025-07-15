780b7cba8fca8180bc7b6023a7ede587a3a2af05
Najrul Islam <mdnajrulislam.4738@gmail.com>
2025-07-15 23:15:12 +0600

    created accessToken

--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -3,6 +3,7 @@ import { IUser } from "../user/user.interface";
 import { User } from "../user/user.mode";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const credentialLogin = async (payload: Partial<IUser>) => {
   const { email, password } = payload;
@@ -22,8 +23,18 @@ const credentialLogin = async (payload: Partial<IUser>) => {
     throw new AppError(httpStatus.BAD_REQUEST, "Password didn't matched.");
   }
 
-  return {
+  const jwtPayload = {
+    userId: isUserExist._id,
     email: isUserExist.email,
+    role: isUserExist.role,
+  };
+
+  const accessToken = await jwt.sign(jwtPayload, "secret", {
+    expiresIn: "1d",
+  });
+
+  return {
+    accessToken,
   };
 };
 
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,10 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { UserController } from "./user.controller";
 import { createUserZodSchema } from "./user.validation";
 import { validateRequest } from "../../middleware/validateRequest";
+import AppError from "../../errorHelpers/AppError";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Role } from "./user.interface";
 
 const router = Router();
 
@@ -10,6 +13,28 @@ router.post(
   validateRequest(createUserZodSchema),
   UserController.createUser
 );
-router.get("/all-users", UserController.getAllUsers);
+router.get(
+  "/all-users",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const accessToken = await req.headers.authorization;
+
+      if (!accessToken) {
+        throw new AppError(403, "No token retrieve");
+      }
+
+      const verifiedToked = await jwt.verify(accessToken, "secret");
+
+      if ((verifiedToked as JwtPayload).role !== Role.ADMIN) {
+        throw new AppError(404, "You are not permitted to view this route");
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+  UserController.getAllUsers
+);
 
 export const UserRoutes = router;
