288f2865a6bb2fb86ad93e8f2673ae04c758f42d
Najrul Islam <mdnajrulislam.4738@gmail.com>
2025-07-20 06:58:36 +0600

    generate new access token using refresh token

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -19,6 +19,24 @@ const credentialLogin = async (
   });
 };
 
+const getNewAccessToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // const refreshToken = req.cookies.refreshToken;
+  const refreshToken = req.headers.authorization;
+  const tokeInfo = await AuthServices.getNewAccessToken(refreshToken as string);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "User logged in successfully.",
+    data: tokeInfo,
+  });
+};
+
 export const AuthControllers = {
   credentialLogin,
+  getNewAccessToken,
 };
--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -1,10 +1,11 @@
 import AppError from "../../errorHelpers/AppError";
-import { IUser } from "../user/user.interface";
+import { IsActive, IUser } from "../user/user.interface";
 import { User } from "../user/user.mode";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
-import { generateToken } from "../../utilities/jwt";
+import { generateToken, verifiedToken } from "../../utilities/jwt";
 import { envVariables } from "../../../config/env";
+import { JwtPayload } from "jsonwebtoken";
 
 const credentialLogin = async (payload: Partial<IUser>) => {
   const { email, password } = payload;
@@ -36,6 +37,59 @@ const credentialLogin = async (payload: Partial<IUser>) => {
     envVariables.JWT_ACCESS_EXPIRES
   );
 
+  const refreshToken = generateToken(
+    jwtPayload,
+    envVariables.JWT_REFRESH_SECRET,
+    envVariables.JWT_REFRESh_EXPIRES
+  );
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { password: pass, ...rest } = isUserExist.toObject();
+
+  return {
+    accessToken,
+    refreshToken,
+    user: rest,
+  };
+};
+const getNewAccessToken = async (refreshToken: string) => {
+  const verifiedRefreshToken = (await verifiedToken(
+    refreshToken,
+    envVariables.JWT_REFRESH_SECRET
+  )) as JwtPayload;
+
+  const isUserExist = await User.findOne({ email: verifiedRefreshToken.email });
+
+  if (!isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User email not exist.");
+  }
+
+  if (
+    isUserExist.isActive === IsActive.BLOCKED ||
+    isUserExist.isActive === IsActive.INACTIVE
+  ) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `User is ${isUserExist.isActive}`
+    );
+  }
+
+  if (isUserExist.isDeleted) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User is deleted");
+  }
+
+  const jwtPayload = {
+    userId: isUserExist._id,
+    email: isUserExist.email,
+    role: isUserExist.role,
+  };
+
+  const accessToken = generateToken(
+    jwtPayload,
+    envVariables.JWT_ACCESS_SECRET,
+    envVariables.JWT_ACCESS_EXPIRES
+  );
+
   return {
     accessToken,
   };
@@ -43,4 +97,5 @@ const credentialLogin = async (payload: Partial<IUser>) => {
 
 export const AuthServices = {
   credentialLogin,
+  getNewAccessToken,
 };
