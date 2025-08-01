231a71ee5cd26da4c88f631654d6878e129b360e
Najrul Islam <mdnajrulislam.4738@gmail.com>
2025-08-01 15:33:46 +0600

    remove general credential login services

--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -1,64 +1,66 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import AppError from "../../errorHelpers/AppError";
-import { IUser } from "../user/user.interface";
+// import { IUser } from "../user/user.interface";
 import { User } from "../user/user.mode";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
 
 import {
   createNewAccessTokenWithRefreshToken,
-  createUserTokens,
+  // createUserTokens,
 } from "../../utilities/userTokens";
 import { JwtPayload } from "jsonwebtoken";
 import { envVariables } from "../../../config/env";
 
-const credentialLogin = async (payload: Partial<IUser>) => {
-  const { email, password } = payload;
+// general credential login implement
+// const credentialLogin = async (payload: Partial<IUser>) => {
+//   const { email, password } = payload;
 
-  const isUserExist = await User.findOne({ email });
+//   const isUserExist = await User.findOne({ email });
 
-  if (!isUserExist) {
-    throw new AppError(httpStatus.BAD_REQUEST, "User email not exist.");
-  }
+//   if (!isUserExist) {
+//     throw new AppError(httpStatus.BAD_REQUEST, "User email not exist.");
+//   }
 
-  const isPasswordMatched = await bcryptjs.compare(
-    password as string,
-    isUserExist.password as string
-  );
+//   const isPasswordMatched = await bcryptjs.compare(
+//     password as string,
+//     isUserExist.password as string
+//   );
 
-  if (!isPasswordMatched) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Password didn't matched.");
-  }
+//   if (!isPasswordMatched) {
+//     throw new AppError(httpStatus.BAD_REQUEST, "Password didn't matched.");
+//   }
 
-  // const jwtPayload = {
-  //   userId: isUserExist._id,
-  //   email: isUserExist.email,
-  //   role: isUserExist.role,
-  // };
+//   // const jwtPayload = {
+//   //   userId: isUserExist._id,
+//   //   email: isUserExist.email,
+//   //   role: isUserExist.role,
+//   // };
 
-  // const accessToken = generateToken(
-  //   jwtPayload,
-  //   envVariables.JWT_ACCESS_SECRET,
-  //   envVariables.JWT_ACCESS_EXPIRES
-  // );
+//   // const accessToken = generateToken(
+//   //   jwtPayload,
+//   //   envVariables.JWT_ACCESS_SECRET,
+//   //   envVariables.JWT_ACCESS_EXPIRES
+//   // );
 
-  // const refreshToken = generateToken(
-  //   jwtPayload,
-  //   envVariables.JWT_REFRESH_SECRET,
-  //   envVariables.JWT_REFRESh_EXPIRES
-  // );
+//   // const refreshToken = generateToken(
+//   //   jwtPayload,
+//   //   envVariables.JWT_REFRESH_SECRET,
+//   //   envVariables.JWT_REFRESh_EXPIRES
+//   // );
 
-  const userTokens = createUserTokens(isUserExist);
+//   const userTokens = createUserTokens(isUserExist);
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { password: pass, ...rest } = isUserExist.toObject();
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   const { password: pass, ...rest } = isUserExist.toObject();
 
-  return {
-    accessToken: userTokens.accessToken,
-    refreshToken: userTokens.refreshToken,
-    user: rest,
-  };
-};
+//   return {
+//     accessToken: userTokens.accessToken,
+//     refreshToken: userTokens.refreshToken,
+//     user: rest,
+//   };
+// };
+
 const getNewAccessToken = async (refreshToken: string) => {
   // const verifiedRefreshToken = (await verifiedToken(
   //   refreshToken,
@@ -130,7 +132,7 @@ const resetPassword = async (
 };
 
 export const AuthServices = {
-  credentialLogin,
+  // credentialLogin,
   getNewAccessToken,
   resetPassword,
 };
