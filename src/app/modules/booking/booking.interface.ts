ece92bcb4897d38c9ebf041ba821675fc32c972f
Najrul Islam <mdnajrulislam.4738@gmail.com>
2025-08-03 22:40:00 +0600

    git pull from development branch and create booking, payment interface and controller

---
+++ src/app/modules/booking/booking.interface.ts
@@ -0,0 +1,18 @@
+// User - Booking(pending) -> Payment (Unpaid) -> SSLCommerz -> Booking update = confirm -> Payment update = paid
+
+import { Types } from "mongoose";
+
+export enum BOOKING_STATUS {
+  PENDING = "PENDING",
+  CANCEL = "CANCEL",
+  COMPLETE = "COMPLETE",
+  FAILED = "FAILED",
+}
+
+export interface IBooking {
+  user: Types.ObjectId;
+  tour: Types.ObjectId;
+  payment?: Types.ObjectId;
+  guestCount: number;
+  status: BOOKING_STATUS;
+}
